f81f1b21e5d5be4bd77c6e3d36adb94783d9c8ca
Seth Lachman <lachmanseth17@gmail.com>
2019-07-31 11:35:43 -0400

    Added ability to save data to file after navigating to server.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const fs = require("fs");
 const router = express.Router();
 const getResults = require("../scraper");
 
 router.get("/", async function(req, res, next) {
-  const result = await getResults();
-  res.send(result);
+  const results = await getResults();
+  let jsonString = JSON.stringify(results);
+  fs.writeFileSync("output.json", jsonString, "utf-8");
+  res.send(results);
 });
 
 module.exports = router;
