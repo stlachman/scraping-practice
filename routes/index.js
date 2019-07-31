53f0aea0d9f05be2cc612b786dfbeaa7b12386f9
Seth Lachman <lachmanseth17@gmail.com>
2019-07-31 17:32:14 -0400

    Extended functionality to scrape multiple pages.

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const router = express.Router();
-const getResults = require("../scraper");
+const fetchData = require("../scraper");
+const siteUrl = "https://newyork.craigslist.org/search/aap";
 
 router.get("/", async function(req, res, next) {
-  const results = await getResults();
-  let jsonString = JSON.stringify(results);
-  fs.writeFileSync("apartments.json", jsonString, "utf-8");
-  fs.readFile("apartments.json", (err, data) => {
-    const result = JSON.parse(data);
-    // console.log(result);
-    for (let key in result) {
-      let price = result[key]["price"].substring(1);
-      if (Number(price) < 1800) {
-        console.log(result[key]);
-      }
-    }
-  });
-  res.send(results);
+  const results = await fetchData(siteUrl);
+  res.send("Compiling data");
 });
 
 module.exports = router;
--- scraper.js
+++ scraper.js
@@ -1,42 +1,93 @@
 const cheerio = require("cheerio");
+const fs = require("fs");
+const fsp = require("fs").promises;
+
 const axios = require("axios");
 
-const siteUrl = "https://newyork.craigslist.org/search/aap";
-
+const outputFile = "apartments.json";
+const lowerPrice = "lower.json";
 const apartments = [];
+let lowerPricePoint = [];
+const pageLimit = 25;
+let pageCounter = 0;
 
-const fetchData = async () => {
-  const result = await axios.get(siteUrl);
-  return cheerio.load(result.data);
-};
+const fetchData = async url => {
+  try {
+    const result = await axios.get(url);
+    const $ = cheerio.load(result.data);
 
-const getResults = async () => {
-  const $ = await fetchData();
+    const apartmentList = $(".content .result-row");
 
-  const apartmentList = $(".content .result-row");
-
-  apartmentList.each(function() {
-    const price = $(this)
-      .find(".result-info .result-price")
-      .text();
-    const title = $(this)
-      .find(".result-info .result-title")
-      .text();
-    const link = $(this)
-      .find(".result-info .result-title")
-      .attr("href");
-    const hood = $(this)
-      .find(".result-info .result-hood")
-      .text();
-    apartments.push({
-      price,
-      title,
-      link,
-      hood
+    apartmentList.each(function() {
+      const price = $(this)
+        .find(".result-info .result-price")
+        .text();
+      const title = $(this)
+        .find(".result-info .result-title")
+        .text();
+      const link = $(this)
+        .find(".result-info .result-title")
+        .attr("href");
+      const hood = $(this)
+        .find(".result-info .result-hood")
+        .text();
+      apartments.push({
+        price,
+        title,
+        link,
+        hood
+      });
     });
-  });
 
-  return apartments;
+    const nextPageLink = $(".paginator")
+      .find(".next.button")
+      .attr("href");
+    pageCounter++;
+
+    if (pageCounter === pageLimit) {
+      exportResults(apartments);
+      return false;
+    }
+
+    fetchData(`https://newyork.craigslist.org${nextPageLink}`);
+  } catch (error) {
+    exportResults(apartments);
+  }
 };
 
-module.exports = getResults;
+const exportResults = apartments => {
+  fs.writeFile(outputFile, JSON.stringify(apartments, null, 4), err => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(`Results exported successfully`);
+  });
+  // fsp
+  //   .readFile("apartments.json", (err, data) => {
+  //     const result = JSON.parse(data);
+  //     for (let key in result) {
+  //       let price = result[key]["price"].substring(1);
+  //       if (Number(price) < 1800) {
+  //         lowerPricePoint.push(result[key]);
+  //         // console.log(result[key]);
+  //       }
+  //     }
+  //     // console.log(lowerPricePoint);
+  //     return lowerPricePoint;
+  //   })
+  //   .then(data => {
+  //     console.log(data);
+  //     fs.writeFile(
+  //       lowerPrice,
+  //       JSON.stringify(lowerPricePoint, null, 4),
+  //       err => {
+  //         if (err) {
+  //           console.log(err);
+  //         }
+  //         console.log(`Prices exported successfully`);
+  //       }
+  //     );
+  //   });
+};
+
+module.exports = fetchData;
